1ce20ba1bcac6f027a611cc22927c201307f44d7
akash sahoo <akashsahoo15595@gmail.com>
2024-07-28 09:05:22 +0530

    rate limit in celery (per task)

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -4,6 +4,9 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dcelery.settings')
 app = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# for rate limit
+app.conf.default_rate_limit = '1/m'
 #app.conf.task_routes = {'newapp.tasks.task1': {'queue':'queue1'}, 'newapp.tasks.task2': {'queue':'queue2'},}
 app.conf.broker_transport_options = {
     'priority_steps': list(range(10)),
